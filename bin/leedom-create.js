2e334cbe685b8999644993bbbb0d55725fecb97b
Leedom <jld1221@163.com>
2024-03-23 11:57:59 +0800

    refactor: optimize init

--- bin/leedom-create.js
+++ bin/leedom-create.js
@@ -16,30 +16,35 @@ program.name('leedom')
   .description('create a new project powered by Leedom')
   .parse(process.argv)
 
-const projectName = program.args[0]
+// 开始执行任务
+init()
 
-if (!projectName) {
-  console.log(error('\n请输入项目名\n'))
-  program.help()
-} else {
-  const list = glob.sync('*') // 遍历当前目录
+// 初始化
+function init() {
+  const projectName = program.args[0]
+  if (!projectName) {
+    console.log(error('\n请输入项目名\n'))
+    program.help()
+  } else {
+    const list = glob.sync('*') // 遍历当前目录
 
-  if (list.length) { // 当前目录不为空
-    // 判断是否有重名文件件
-    const hasDuplicateNameDir = list.filter((name) => {
-      const fileName = path.resolve(process.cwd(), path.join('.', name))
-      const isDir = fs.statSync(fileName).isDirectory()
-      return name === projectName && isDir
-    })
+    if (list.length) { // 当前目录不为空
+      // 判断是否有重名文件件
+      const hasDuplicateNameDir = list.filter((name) => {
+        const fileName = path.resolve(process.cwd(), path.join('.', name))
+        const isDir = fs.statSync(fileName).isDirectory()
+        return name === projectName && isDir
+      })
 
-    if (hasDuplicateNameDir.length) {
-      const existDirectoryName = path.resolve(process.cwd(), path.join('.', hasDuplicateNameDir[0]))
-      console.log(`当前目录 ${info(existDirectoryName)} 已经存在，请另起项目名!`)
+      if (hasDuplicateNameDir.length) {
+        const existDirectoryName = path.resolve(process.cwd(), path.join('.', hasDuplicateNameDir[0]))
+        console.log(`当前目录 ${info(existDirectoryName)} 已经存在，请另起项目名!`)
+      } else {
+        generateProject(projectName)
+      }
     } else {
       generateProject(projectName)
     }
-  } else {
-    generateProject(projectName)
   }
 }
 
