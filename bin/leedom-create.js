2472a3d1505d5a4b0fc998a4721ca1d865b7813c
Leedom <jld1221@163.com>
2024-03-23 22:13:27 +0800

    feat: default project name and optimize ux

--- bin/leedom-create.js
+++ bin/leedom-create.js
@@ -20,37 +20,49 @@ program.name('leedom')
 init()
 
 // 初始化
-function init() {
-  const projectName = program.args[0]
+async function init() {
+  console.clear()
+  console.log(info(`\n${name} v${version}\n`))
+  let projectName = program.args[0]
   if (!projectName) {
-    console.log(error('\n请输入项目名\n'))
-    program.help()
-  } else {
-    const list = glob.sync('*') // 遍历当前目录
+    const { action } = await inquirer.prompt([
+      {
+        name: 'action',
+        type: 'input',
+        message: '请输入您的项目名:',
+        default: 'my-project',
+      },
+    ])
+    projectName = action
+  }
+  checkDuplicateDir(projectName)
+}
 
-    if (list.length) { // 当前目录不为空
-      // 判断是否有重名文件件
-      const hasDuplicateNameDir = list.filter((name) => {
-        const fileName = path.resolve(process.cwd(), path.join('.', name))
-        const isDir = fs.statSync(fileName).isDirectory()
-        return name === projectName && isDir
-      })
+// 检查项目名是否重复
+function checkDuplicateDir(projectName) {
+  const list = glob.sync('*') // 遍历当前目录
 
-      if (hasDuplicateNameDir.length) {
-        const existDirectoryName = path.resolve(process.cwd(), path.join('.', hasDuplicateNameDir[0]))
-        console.log(`当前目录 ${info(existDirectoryName)} 已经存在，请另起项目名!`)
-      } else {
-        generateProject(projectName)
-      }
+  if (list.length) { // 当前目录不为空
+    // 判断是否有重名文件件
+    const hasDuplicateNameDir = list.filter((name) => {
+      const fileName = path.resolve(process.cwd(), path.join('.', name))
+      const isDir = fs.statSync(fileName).isDirectory()
+      return name === projectName && isDir
+    })
+
+    if (hasDuplicateNameDir.length) {
+      const existDirectoryName = path.resolve(process.cwd(), path.join('.', hasDuplicateNameDir[0]))
+      console.log(`当前目录 ${info(existDirectoryName)} 已经存在，请另起项目名!`)
     } else {
       generateProject(projectName)
     }
+  } else {
+    generateProject(projectName)
   }
 }
 
 // 创建项目
 async function generateProject(projectName) {
-  console.log(info(`${name} v${version}\n`))
   const { action } = await inquirer.prompt([
     {
       name: 'action',
